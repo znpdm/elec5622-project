8a0f247525adf39907d47f7d701034a53369d828
znpdm <nzha9510@uni.sydney.edu.au>
2018-08-27 02:51:38 -0400

    test2

--- inputfilename.c
+++ inputfilename.c
@@ -9,7 +9,7 @@
 int main()
 {
 	DB *dbp;
-	DBT key, data;
+	DBT key, data,DOB;
 	int ret, t_ret;
 
 	if ((ret = db_create(&dbp, NULL, 0)) != 0) {
@@ -24,11 +24,14 @@ int main()
 
 	memset(&key, 0, sizeof(key));
 	memset(&data, 0, sizeof(data));
+	memset(&DOB,0,sizeof(DOB));
 
 	key.data = "Chen";
 	key.size = sizeof("Chen");
 	data.data = "Jianhui";
 	data.size = sizeof("Jianhui");
+	DOB.data = "19/10";
+	DOB.size = sizeof("19/10");
 
 	if ((ret = dbp->put(dbp, NULL, &key, &data, 0)) == 0)
 		printf("db: %s: key stored.\n", (char *)key.data);
@@ -39,6 +42,13 @@ int main()
 	if ((ret = dbp->get(dbp, NULL, &key, &data, 0)) == 0)
 		printf("db: %s: key retrieved: data was %s.\n",
 		    (char *)key.data, (char *)data.data);
+	else {
+		dbp->err(dbp, ret, "DB->get");
+		goto err;
+	}
+	if ((ret = dbp->get(dbp, NULL, &key, &data, 0)) == 0)
+		printf("db: %s: key retrieved: DOB was %s.\n",
+		    (char *)key.data, (char *)DOB.data);
 	else {
 		dbp->err(dbp, ret, "DB->get");
 		goto err;
